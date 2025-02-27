337f1d1d1b6f26d9797d31534c282775b6d6352d
Votvlad <vlad.ainur@gmail.com>
2025-02-27 23:11:38 +0300

    "Встроенные функции, lesson 13.1"

---
+++ Task_13.py
@@ -0,0 +1,31 @@
+"""
+string = 'brothers'
+number = 123
+my_tuple = ('18', 24, 'Gustavo')
+
+print(id(string))
+print(id(number))
+print(id(my_tuple))
+print()
+print(hash(string))
+print(hash(number))
+print(hash(my_tuple))
+print()
+print(dir(string))
+print(dir(number))
+print(dir(my_tuple))
+"""
+
+"""
+list_1 = [1, 2, 3]
+list_2 = ['Mike', 'Todd', 'Gus']
+list_combo = zip(list_1, list_2)  #list_combo = list(zip(list_1, list_2))
+print(list(list_combo))
+
+list_num = [12, 567, 81, 9, -45]
+print(max(list_num))
+print(min(list_num))
+print(sum(list_num))
+"""
+
+
