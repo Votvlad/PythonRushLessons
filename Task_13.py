47b850ff2d00466a70d784c28d29632d21a6ca2d
Votvlad <vlad.ainur@gmail.com>
2025-03-02 23:20:08 +0300

    "Вся мощь декораторов, lesson 14.2"

--- Task_13.py
+++ Task_13.py
@@ -133,4 +133,50 @@ def say_hello(name):
 say_hello('Kim')
 """
 
+"""
+def decorator1(func):
+    def wrapper(*args, **kwargs):
+        print("Decorator 1: Before function call")
+        result = func(*args, **kwargs)
+        print("Decorator 1: After function call")
+        return result
+    return wrapper
+
+def decorator2(func):
+    def wrapper(*args, **kwargs):
+        print("Decorator 2: Before function call")
+        result = func(*args, **kwargs)
+        print("Decorator 2: After function call")
+        return result
+    return wrapper
+
+@decorator1
+@decorator2
+def say_hello():
+    print("Hello, world!")
+say_hello()
+"""
+
+"""
+import time
+
+def time_decorator(func):
+    def wrapper(n):
+        start_time = time.time()
+        result = func(n)
+        end_time = time.time()
+        execution_time = end_time - start_time
+        print(f"Функция {func.__name__} выполнилась за {execution_time:.4f} секунд")
+        return result
+    return wrapper
+
+@time_decorator
+def compute_square(n):
+    time.sleep(0.5)  # Имитация задержки
+    return n * n
+
+result = compute_square(5)
+print(f"Результат вычисления: {result}")
+"""
+
 
