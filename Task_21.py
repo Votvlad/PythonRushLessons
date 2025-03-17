ac8293ee32ff4462a05188b16315928970039999
Votvlad <vlad.ainur@gmail.com>
2025-03-17 19:26:30 +0300

    "Чтение данных из файла", lesson 21.1

--- Task_21.py
+++ Task_21.py
@@ -17,4 +17,21 @@ file.write("\nAppended text.")
 file.close()
 """
 
+# Чтение данных из файла
 
+"""
+file = open('mypackage/example.txt', 'r')
+lines = file.readlines()
+for line in lines:
+    print(line.strip())
+file.close()
+"""
+
+"""
+file = open('mypackage/example.txt', 'r')
+line = file.readline()
+while line:
+    print(line.strip())
+    line = file.readline()
+file.close()
+"""
