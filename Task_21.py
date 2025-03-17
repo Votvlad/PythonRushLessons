5232a4d888c0e81c96319cd4e2ab2a593e727ed6
Votvlad <vlad.ainur@gmail.com>
2025-03-17 16:17:35 +0300

    "Работа с файлами", lesson 21.0

---
+++ Task_21.py
@@ -0,0 +1,20 @@
+# Работа с файлами
+
+"""
+file = open('mypackage/example.txt', 'r')
+content = file.read()
+print(content)
+file.close()
+"""
+
+"""
+file = open('mypackage/example.txt', 'w')
+file.write("Hello, World!")
+file.close()
+
+file = open('mypackage/example.txt', 'a')
+file.write("\nAppended text.")
+file.close()
+"""
+
+
